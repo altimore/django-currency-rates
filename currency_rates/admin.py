472df32a2363d1e1511956c972dd8a06f79ff9f8
Altimore <altimore9991@gmail.com>
2016-08-16 11:28:49 +0400

    Relative import.

--- currency_rates/admin.py
+++ currency_rates/admin.py
@@ -1,16 +1,19 @@
 from django.contrib import admin
-from models import Currency, ExchangeRate
+
+from .models import Currency, ExchangeRate
 
 
 class CurrencyAdmin(admin.ModelAdmin):
     list_display = ('code', 'name', 'current_rate', 'is_default')
-    list_filter = ('is_default',)
-    search_fields = ('code', 'name') 
+    list_filter = ('is_default', )
+    search_fields = ('code', 'name')
+
 
 class ExchangeRateAdmin(admin.ModelAdmin):
     list_display = ('currency', 'date', 'rate')
-    list_filter = ('date',)
-    search_fields = ('currency__code', 'currency__name') 
+    list_filter = ('date', )
+    search_fields = ('currency__code', 'currency__name')
+
 
 admin.site.register(Currency, CurrencyAdmin)
 admin.site.register(ExchangeRate, ExchangeRateAdmin)
