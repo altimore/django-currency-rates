d00799c6e0f6ef7eafa58fedb08b61e36b77861d
Thomas Couderette <thomas.couderette@sodibur.com>
2022-08-16 12:12:54 +0400

    fix: made the import relative to the current folder

--- currency_rates/models.py
+++ currency_rates/models.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from converters import get_rate
+from .converters import get_rate
 
 
 def default_currency():
