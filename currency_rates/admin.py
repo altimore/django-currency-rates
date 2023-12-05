3d6758db463b95035e29d571121560217d480bb4
Thomas Couderette <thomas.couderette@sodibur.com>
2023-12-05 13:31:08 +0400

    bump: version 0.13.2 → 0.13.3

--- currency_rates/admin.py
+++ currency_rates/admin.py
@@ -3,17 +3,15 @@ from django.contrib import admin
 from .models import Currency, ExchangeRate
 
 
+@admin.register(Currency)
 class CurrencyAdmin(admin.ModelAdmin):
-    list_display = ("code", "name", "current_rate", "is_default")
+    list_display = ("code", "name", "is_default")
     list_filter = ("is_default",)
     search_fields = ("code", "name")
 
 
+@admin.register(ExchangeRate)
 class ExchangeRateAdmin(admin.ModelAdmin):
     list_display = ("currency_sold", "currency_bought", "date", "rate")
     list_filter = ("date",)
     search_fields = ("currency__code", "currency__name")
-
-
-admin.site.register(Currency, CurrencyAdmin)
-admin.site.register(ExchangeRate, ExchangeRateAdmin)
